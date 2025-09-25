1535cf0a4ab647c132d8c8fd572162cda7c7ff54
MuhammadUmarMughalAzeemi <m.hussain1052@gmail.com>
2025-09-25 14:44:16 +0500

    changes in storage directory

--- storage/framework/views/1f8dfa10e3fa3c3c5592b0037b00f4c39a52e1a6.php
+++
@@ -1,5 +0,0 @@
-<p <?php echo e($attributes->class(['filament-tables-header-description'])); ?>>
-    <?php echo e($slot); ?>
-
-</p>
-<?php /**PATH C:\laragon\www\bs-nursing\vendor\filament\tables\src\/../resources/views/components/header/description.blade.php ENDPATH**/ ?>
--- storage/framework/views/30dff3de82b1b1b286b277fd8db3b7d5b9d831c6.php
+++
@@ -1,8 +0,0 @@
-<h2
-    <?php echo e($attributes->class(['filament-tables-header-heading text-xl font-bold tracking-tight'])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</h2>
-<?php /**PATH C:\laragon\www\bs-nursing\vendor\filament\tables\src\/../resources/views/components/header/heading.blade.php ENDPATH**/ ?>
